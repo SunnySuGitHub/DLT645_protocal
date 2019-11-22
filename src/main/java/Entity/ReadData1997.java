cbe42781f24ae93e6cb87818fdc563ab066cc51d
SunnySuGitHub <499974664@qq.com>
2019-11-22 10:40:19 +0800

    xiugai

--- src/main/java/Entity/ReadData1997.java
+++ src/main/java/Entity/ReadData1997.java
@@ -21,6 +21,11 @@ public class ReadData1997 {
     private String dataType;          //数据标识
 
     public ReadData1997(byte[] bytes) {
+        /**
+         * 起始符  地址域  起始符  控制码  数据域长度              数据域                校验码   结束符
+         *  0       1~6      7      8         9                   9+n
+         * 1字节   6字节   1字节   1字节     1字节         n字节(2字节数据标识+数据)      1字节   1字节
+         */
         this.deviceAddress = CheckUtil.GetBCDAddress(bytes);
         boolean check = CheckUtil.checkData(bytes);
         if (!check) {
@@ -46,133 +51,6 @@ public class ReadData1997 {
             this.effectiveData = data;
             this.dataType = DataIdentify1997.getIdentify_Name().get(Arrays.toString(type)); //获取数据标识
             this.dot = DataIdentify1997.getDot().get(Arrays.toString(type));
-            //        switch (read_ints[8])//read_int[8]为控制码
-//        {
-//            case 0x81:
-//                /**
-//                 * 起始符  地址域  起始符  控制码  数据域长度              数据域                校验码   结束符
-//                 *  0       1~6      7      8         9                   9+n
-//                 * 1字节   6字节   1字节   1字节     1字节         n字节(2字节数据标识+数据)      1字节   1字节
-//                 */
-//                int lengthOfData = read_ints[9]; //数据域长度
-//                byte[] type = new byte[2];   // 两字节的数据标识
-//                int[] data = new int[lengthOfData - 2]; //除去两字节的数据标识剩下的数据长度
-//                for (int t = 0; t < 2; t++) {
-//                    type[t] = (byte) (read_ints[10 + t] - 0x33); //构建数据标识 DI0 DI1
-//                }
-//                for (int d = 0; d < lengthOfData - 2; d++) {
-//                    data[d] = read_ints[12 + d] - 0x33;// 获取真正数据域
-//                    data[d] = Integer.valueOf(ConvertUtil.intToHex(data[d]));
-//                }
-//                this.effectiveData = data;
-//                this.dataType = DataIdentify1997.getIdentify_Name().get(Arrays.toString(type)); //获取数据标识
-//                this.dot = DataIdentify1997.getDot().get(Arrays.toString(type));
-//                StringBuilder sb = new StringBuilder();
-//                for (int i = data.length - 1; i >= 0; i--) {
-//                    sb.append(data[i]);
-//                }
-//                sb.insert(sb.length() - dot, ".");
-//                String res = sb.toString();
-//                double dataresult = Double.valueOf(res);
-//                String str_addr = ConvertUtil.ToHex(bytes);
-//                System.out.println(TimeUtil.getCurrentTime() + " Received  " + dataType + " from " + deviceAddress + "  Client : " + str_addr + "\n");
-//                System.out.println("*******************" + dataType + " : " + dataresult + "***********************\n");
-//                //WriteDataToDatabase writeToDataBase = new WriteDataToDatabase();
-////                        writeToDataBase.write(dataType, dataresult, deviceAddress);
-//                Random random = new Random();
-//                double voltage = 210 + 20.0 * random.nextFloat();
-//                double inf = 0.8 + 0.1 * random.nextFloat();
-//
-//                switch (dataType) {
-//                    case "a_positive_power":
-//                        System.out.println(TimeUtil.getCurrentTime() + " Received  " + "a_influence" + " from " + deviceAddress + "  Client : " + str_addr + "\n");
-//                        System.out.println("*******************" + "a_influence" + " : " + inf + "***********************\n");
-////                                writeToDataBase.write("a_influence", inf, deviceAddress);
-//
-//                        System.out.println(TimeUtil.getCurrentTime() + " Received  " + "a_apparent_power" + " from " + deviceAddress + "  Client : " + str_addr + "\n");
-//                        System.out.println("*******************" + "a_apparent_power" + " : " + dataresult / inf + "***********************\n");
-////                                writeToDataBase.write("a_apparent_power", dataresult/inf, deviceAddress);
-//
-//                        double gou = Math.sqrt(1.0 - inf * inf);
-//                        System.out.println(TimeUtil.getCurrentTime() + " Received  " + "a_reactive_power" + " from " + deviceAddress + "  Client : " + str_addr + "\n");
-//                        System.out.println("*******************" + "a_reactive_power" + " : " + dataresult * gou + "***********************\n");
-////                                writeToDataBase.write("a_reactive_power", dataresult*gou, deviceAddress);
-//
-//                        System.out.println(TimeUtil.getCurrentTime() + " Received  " + "a_voltage" + " from " + deviceAddress + "  Client : " + str_addr + "\n");
-//                        System.out.println("*******************" + "a_voltage" + " : " + voltage + "***********************\n");
-////                                writeToDataBase.write("a_voltage", voltage, deviceAddress);
-//
-//                        System.out.println(TimeUtil.getCurrentTime() + " Received  " + "a_current" + " from " + deviceAddress + "  Client : " + str_addr + "\n");
-//                        System.out.println("*******************" + "a_current" + " : " + (1000 * dataresult / voltage) + "***********************\n");
-////                                writeToDataBase.write("a_current", 1000 * dataresult / voltage, deviceAddress);
-//
-//                        break;
-//                    case "b_positive_power":
-//                        System.out.println(TimeUtil.getCurrentTime() + " Received  " + "b_influence" + " from " + deviceAddress + "  Client : " + str_addr + "\n");
-//                        System.out.println("*******************" + "b_influence" + " : " + inf + "***********************\n");
-////                                writeToDataBase.write("b_influence", inf, deviceAddress);
-//
-//                        System.out.println(TimeUtil.getCurrentTime() + " Received  " + "b_apparent_power" + " from " + deviceAddress + "  Client : " + str_addr + "\n");
-//                        System.out.println("*******************" + "b_apparent_power" + " : " + dataresult / inf + "***********************\n");
-////                                writeToDataBase.write("b_apparent_power", dataresult/inf, deviceAddress);
-//
-//                        gou = Math.sqrt(1.0 - inf * inf) / inf;
-//                        System.out.println(TimeUtil.getCurrentTime() + " Received  " + "b_reactive_power" + " from " + deviceAddress + "  Client : " + str_addr + "\n");
-//                        System.out.println("*******************" + "b_reactive_power" + " : " + dataresult / gou + "***********************\n");
-////                                writeToDataBase.write("b_reactive_power", dataresult*gou, deviceAddress);
-//
-//                        System.out.println(TimeUtil.getCurrentTime() + " Received  " + "b_voltage" + " from " + deviceAddress + "  Client : " + str_addr + "\n");
-//                        System.out.println("*******************" + "b_voltage" + " : " + voltage + "***********************\n");
-////                                writeToDataBase.write("b_voltage", voltage, deviceAddress);
-//
-//                        System.out.println(TimeUtil.getCurrentTime() + " Received  " + "b_current" + " from " + deviceAddress + "  Client : " + str_addr + "\n");
-//                        System.out.println("*******************" + "b_current" + " : " + (1000 * dataresult / voltage) + "***********************\n");
-////                                writeToDataBase.write("b_current", 1000 * dataresult / voltage, deviceAddress);
-//                        break;
-//                    case "c_positive_power":
-//                        System.out.println(TimeUtil.getCurrentTime() + " Received  " + "c_influence" + " from " + deviceAddress + "  Client : " + str_addr + "\n");
-//                        System.out.println("*******************" + "c_influence" + " : " + inf + "***********************\n");
-////                                writeToDataBase.write("c_influence", inf, deviceAddress);
-//
-//                        System.out.println(TimeUtil.getCurrentTime() + " Received  " + "c_apparent_power" + " from " + deviceAddress + "  Client : " + str_addr + "\n");
-//                        System.out.println("*******************" + "c_apparent_power" + " : " + dataresult / inf + "***********************\n");
-////                                writeToDataBase.write("c_apparent_power", dataresult/inf, deviceAddress);
-//
-//                        gou = Math.sqrt(1.0 - inf * inf) / inf;
-//                        System.out.println(TimeUtil.getCurrentTime() + " Received  " + "c_reactive_power" + " from " + deviceAddress + "  Client : " + str_addr + "\n");
-//                        System.out.println("*******************" + "c_reactive_power" + " : " + dataresult * gou + "***********************\n");
-////                                writeToDataBase.write("c_reactive_power", dataresult*gou, deviceAddress);
-//
-//                        System.out.println(TimeUtil.getCurrentTime() + " Received  " + "c_voltage" + " from " + deviceAddress + "  Client : " + str_addr + "\n");
-//                        System.out.println("*******************" + "c_voltage" + " : " + voltage + "***********************\n");
-////                                writeToDataBase.write("c_voltage", voltage, deviceAddress);
-//
-//                        System.out.println(TimeUtil.getCurrentTime() + " Received  " + "c_current" + " from " + deviceAddress + "  Client : " + str_addr + "\n");
-//                        System.out.println("*******************" + "c_current" + " : " + (1000 * dataresult / voltage) + "***********************\n");
-////                                writeToDataBase.write("c_current", 1000 * dataresult / voltage, deviceAddress);
-//                        break;
-//                    case "positive_power":
-//                        System.out.println(TimeUtil.getCurrentTime() + " Received  " + "influence" + " from " + deviceAddress + "  Client : " + str_addr + "\n");
-//                        System.out.println("*******************" + "influence" + " : " + inf + "***********************\n");
-////                                writeToDataBase.write("influence", inf, deviceAddress);
-//
-//                        System.out.println(TimeUtil.getCurrentTime() + " Received  " + "apparent_power" + " from " + deviceAddress + "  Client : " + str_addr + "\n");
-//                        System.out.println("*******************" + "apparent_power" + " : " + dataresult / inf + "***********************\n");
-////                                writeToDataBase.write("apparent_power", dataresult/inf, deviceAddress);
-//
-//                        gou = Math.sqrt(1.0 - inf * inf) / inf;
-//                        System.out.println(TimeUtil.getCurrentTime() + " Received  " + "reactive_power" + " from " + deviceAddress + "  Client : " + str_addr + "\n");
-//                        System.out.println("*******************" + "reactive_power" + " : " + dataresult * gou + "***********************\n");
-////                                writeToDataBase.write("reactive_power", dataresult*gou, deviceAddress);
-//
-//                        System.out.println(TimeUtil.getCurrentTime() + " Received  " + "wave" + " from " + deviceAddress + "  Client : " + str_addr + "\n");
-//                        System.out.println("*******************" + "wave" + " : " + dataresult / inf + "***********************\n");
-////                                writeToDataBase.write("wave", inf*5000, deviceAddress);
-//                    default:
-//
-//                }
-//            default:
-//        }
         }
     }
 
--- src/main/java/Handler/MessageHandler.java
+++ src/main/java/Handler/MessageHandler.java
@@ -1,16 +1,10 @@
 package Handler;
 
-import Entity.DataIdentify1997;
 import Entity.ReadData1997;
-import Utils.ConvertUtil;
 import Utils.TimeUtil;
 import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelPromise;
-
-import java.util.Arrays;
-import java.util.Random;
 
 /**
  * @author: suxinyu
@@ -23,108 +17,57 @@ public class MessageHandler extends ChannelHandlerAdapter {
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         ReadData1997 readData = (ReadData1997) msg;
         switch (readData.getControlCode()){
-            case 0x81:
-                /**
-                 * 起始符  地址域  起始符  控制码  数据域长度              数据域                校验码   结束符
-                 *  0       1~6      7      8         9                   9+n
-                 * 1字节   6字节   1字节   1字节     1字节         n字节(2字节数据标识+数据)      1字节   1字节
-                 */
-                int[] data = readData.getEffectiveData();
-                StringBuilder sb = new StringBuilder();
-                for (int i = data.length - 1; i >= 0; i--) {
-                    sb.append(data[i]);
-                }
-                sb.insert(sb.length() - readData.getDot(), ".");
-                double dataresult = Double.valueOf(sb.toString());
-                System.out.println(TimeUtil.getCurrentTime() + " Received  " + readData.getDataType() + " from " + readData.getDeviceAddress());
-                System.out.println("*********************Value: " + dataresult + "*********************");
-//                switch (readData.getDataType()) {
-//                    case "a_positive_power":
-//                        System.out.println(TimeUtil.getCurrentTime() + " Received  " + "a_influence" + " from " + deviceAddress + "  Client : " + str_addr + "\n");
-//                        System.out.println("*******************" + "a_influence" + " : " + inf + "***********************\n");
-////                                writeToDataBase.write("a_influence", inf, deviceAddress);
-//
-//                        System.out.println(TimeUtil.getCurrentTime() + " Received  " + "a_apparent_power" + " from " + deviceAddress + "  Client : " + str_addr + "\n");
-//                        System.out.println("*******************" + "a_apparent_power" + " : " + dataresult / inf + "***********************\n");
-////                                writeToDataBase.write("a_apparent_power", dataresult/inf, deviceAddress);
-//
-//                        double gou = Math.sqrt(1.0 - inf * inf);
-//                        System.out.println(TimeUtil.getCurrentTime() + " Received  " + "a_reactive_power" + " from " + deviceAddress + "  Client : " + str_addr + "\n");
-//                        System.out.println("*******************" + "a_reactive_power" + " : " + dataresult * gou + "***********************\n");
-////                                writeToDataBase.write("a_reactive_power", dataresult*gou, deviceAddress);
-//
-//                        System.out.println(TimeUtil.getCurrentTime() + " Received  " + "a_voltage" + " from " + deviceAddress + "  Client : " + str_addr + "\n");
-//                        System.out.println("*******************" + "a_voltage" + " : " + voltage + "***********************\n");
-////                                writeToDataBase.write("a_voltage", voltage, deviceAddress);
-//
-//                        System.out.println(TimeUtil.getCurrentTime() + " Received  " + "a_current" + " from " + deviceAddress + "  Client : " + str_addr + "\n");
-//                        System.out.println("*******************" + "a_current" + " : " + (1000 * dataresult / voltage) + "***********************\n");
-////                                writeToDataBase.write("a_current", 1000 * dataresult / voltage, deviceAddress);
-//
-//                        break;
-//                    case "b_positive_power":
-//                        System.out.println(TimeUtil.getCurrentTime() + " Received  " + "b_influence" + " from " + deviceAddress + "  Client : " + str_addr + "\n");
-//                        System.out.println("*******************" + "b_influence" + " : " + inf + "***********************\n");
-////                                writeToDataBase.write("b_influence", inf, deviceAddress);
-//
-//                        System.out.println(TimeUtil.getCurrentTime() + " Received  " + "b_apparent_power" + " from " + deviceAddress + "  Client : " + str_addr + "\n");
-//                        System.out.println("*******************" + "b_apparent_power" + " : " + dataresult / inf + "***********************\n");
-////                                writeToDataBase.write("b_apparent_power", dataresult/inf, deviceAddress);
-//
-//                        gou = Math.sqrt(1.0 - inf * inf) / inf;
-//                        System.out.println(TimeUtil.getCurrentTime() + " Received  " + "b_reactive_power" + " from " + deviceAddress + "  Client : " + str_addr + "\n");
-//                        System.out.println("*******************" + "b_reactive_power" + " : " + dataresult / gou + "***********************\n");
-////                                writeToDataBase.write("b_reactive_power", dataresult*gou, deviceAddress);
-//
-//                        System.out.println(TimeUtil.getCurrentTime() + " Received  " + "b_voltage" + " from " + deviceAddress + "  Client : " + str_addr + "\n");
-//                        System.out.println("*******************" + "b_voltage" + " : " + voltage + "***********************\n");
-////                                writeToDataBase.write("b_voltage", voltage, deviceAddress);
-//
-//                        System.out.println(TimeUtil.getCurrentTime() + " Received  " + "b_current" + " from " + deviceAddress + "  Client : " + str_addr + "\n");
-//                        System.out.println("*******************" + "b_current" + " : " + (1000 * dataresult / voltage) + "***********************\n");
-////                                writeToDataBase.write("b_current", 1000 * dataresult / voltage, deviceAddress);
-//                        break;
-//                    case "c_positive_power":
-//                        System.out.println(TimeUtil.getCurrentTime() + " Received  " + "c_influence" + " from " + deviceAddress + "  Client : " + str_addr + "\n");
-//                        System.out.println("*******************" + "c_influence" + " : " + inf + "***********************\n");
-////                                writeToDataBase.write("c_influence", inf, deviceAddress);
-//
-//                        System.out.println(TimeUtil.getCurrentTime() + " Received  " + "c_apparent_power" + " from " + deviceAddress + "  Client : " + str_addr + "\n");
-//                        System.out.println("*******************" + "c_apparent_power" + " : " + dataresult / inf + "***********************\n");
-////                                writeToDataBase.write("c_apparent_power", dataresult/inf, deviceAddress);
-//
-//                        gou = Math.sqrt(1.0 - inf * inf) / inf;
-//                        System.out.println(TimeUtil.getCurrentTime() + " Received  " + "c_reactive_power" + " from " + deviceAddress + "  Client : " + str_addr + "\n");
-//                        System.out.println("*******************" + "c_reactive_power" + " : " + dataresult * gou + "***********************\n");
-////                                writeToDataBase.write("c_reactive_power", dataresult*gou, deviceAddress);
-//
-//                        System.out.println(TimeUtil.getCurrentTime() + " Received  " + "c_voltage" + " from " + deviceAddress + "  Client : " + str_addr + "\n");
-//                        System.out.println("*******************" + "c_voltage" + " : " + voltage + "***********************\n");
-////                                writeToDataBase.write("c_voltage", voltage, deviceAddress);
-//
-//                        System.out.println(TimeUtil.getCurrentTime() + " Received  " + "c_current" + " from " + deviceAddress + "  Client : " + str_addr + "\n");
-//                        System.out.println("*******************" + "c_current" + " : " + (1000 * dataresult / voltage) + "***********************\n");
-////                                writeToDataBase.write("c_current", 1000 * dataresult / voltage, deviceAddress);
-//                        break;
-//                    case "positive_power":
-//                        System.out.println(TimeUtil.getCurrentTime() + " Received  " + "influence" + " from " + deviceAddress + "  Client : " + str_addr + "\n");
-//                        System.out.println("*******************" + "influence" + " : " + inf + "***********************\n");
-////                                writeToDataBase.write("influence", inf, deviceAddress);
-//
-//                        System.out.println(TimeUtil.getCurrentTime() + " Received  " + "apparent_power" + " from " + deviceAddress + "  Client : " + str_addr + "\n");
-//                        System.out.println("*******************" + "apparent_power" + " : " + dataresult / inf + "***********************\n");
-////                                writeToDataBase.write("apparent_power", dataresult/inf, deviceAddress);
-//
-//                        gou = Math.sqrt(1.0 - inf * inf) / inf;
-//                        System.out.println(TimeUtil.getCurrentTime() + " Received  " + "reactive_power" + " from " + deviceAddress + "  Client : " + str_addr + "\n");
-//                        System.out.println("*******************" + "reactive_power" + " : " + dataresult * gou + "***********************\n");
-////                                writeToDataBase.write("reactive_power", dataresult*gou, deviceAddress);
-//
-//                        System.out.println(TimeUtil.getCurrentTime() + " Received  " + "wave" + " from " + deviceAddress + "  Client : " + str_addr + "\n");
-//                        System.out.println("*******************" + "wave" + " : " + dataresult / inf + "***********************\n");
-////                                writeToDataBase.write("wave", inf*5000, deviceAddress);
-//            case 0x82:
+            case 0x81: //主站请求读数据应答后 -- 无后续数据帧情况
+                double dataresult = getDataresult(readData);
+                System.out.println(TimeUtil.getCurrentTime() + " 主站请求读数据后" + "收到无后续数据帧情况 from "
+                        + readData.getDeviceAddress()
+                        + " " + readData.getDataType()
+                        + " = " + dataresult);
+                break;
+            case 0xA1: //主站请求读数据应答后 --> 有后续数据帧情况，需要再次请求后续数据
+                System.out.println(TimeUtil.getCurrentTime() + " 主站请求读数据后" + "收到有后续数据帧情况 from "
+                        + readData.getDeviceAddress()
+                        + " " + readData.getDataType()
+                        + " = ");
+                break;
+            case 0x82: //主站读后续数据请求应答后 --> 无后续数据帧情况
+                System.out.println(TimeUtil.getCurrentTime() + " 主站请求读后续数据后" + "收到无后续数据帧情况 from "
+                        + readData.getDeviceAddress()
+                        + " " + readData.getDataType()
+                        + " = ");
+                break;
+            case 0xA2: //主站读后续数据请求应答后 --> 有后续数据帧情况
+                System.out.println(TimeUtil.getCurrentTime() + " 主站请求读后续数据后" + "收到有后续数据帧情况 from "
+                        + readData.getDeviceAddress()
+                        + " " + readData.getDataType()
+                        + " = ");
+                break;
+            case 0x83: //主站重读数据请求应答后 --> 无后续数据帧情况
+                System.out.println(TimeUtil.getCurrentTime() + " 主站请求重读数据后" + "收到无后续数据帧情况 from "
+                        + readData.getDeviceAddress()
+                        + " " + readData.getDataType()
+                        + " = ");
+                break;
+            case 0xA3: //主站重读数据请求应答后 --> 有后续数据帧情况
+                System.out.println(TimeUtil.getCurrentTime() + " 主站请求重读数据后" + "收到有后续数据帧情况 from "
+                        + readData.getDeviceAddress()
+                        + " " + readData.getDataType()
+                        + " = ");
+                break;
+                //写数据、广播校时、写设备地址、更改通信速率、修改密码、最大需量清零未实现
+
+
         }
+    }
+
+    private double getDataresult(ReadData1997 readData) {
+        int[] data = readData.getEffectiveData();
+        StringBuilder sb = new StringBuilder();
+        for (int i = data.length - 1; i >= 0; i--) {
+            sb.append(data[i]);
+        }
+        sb.insert(sb.length() - readData.getDot(), ".");
+        return Double.valueOf(sb.toString());
     }
 
     @Override
